4c1fe7adc69c2cea908e2579145bbcc7bbf5b8a2
JohanaBaez <johanarabaez617@gmail.com>
2022-10-04 22:10:49 -0600

    creacion de carpetas del proyecto y instalacion de dependencias para la emulacion en la web

---
+++ src/components/ScrollView.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+const ScrollView = ()=>{
+
+    return(
+        <view>
+            <Text>
+                Hola mundox
+            </Text>
+        </view>
+    )
+}
+
+export default ScrollView;
+
+const styles = StyleSheet.create({
+
+});
+
